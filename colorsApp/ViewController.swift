585fc75f390bf759b591f27c5caa205159de0a09
Ana Caroline Freitas Sampaio <ana.sampaio@br-ifce-108.local>
2018-11-20 14:06:42 -0300

    Initial Commit

---
+++ colorsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  colorsApp
+//
+//  Created by Ana Caroline Freitas Sampaio on 20/11/2018.
+//  Copyright © 2018 Ana Caroline Freitas Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
